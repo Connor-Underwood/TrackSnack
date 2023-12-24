75b82e7efff13665b18a3ae9a37f0322d33b426a
Connor Underwood <underw23@purdue.edu>
2023-12-23 21:51:43 -0600

    added dashboard and foodlog

--- TrackSnack/App.js
+++ TrackSnack/App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
+import FoodLogScreen from './screens/FoodLogScreen'
 import RegisterScreen from './screens/RegisterScreen';
+import DashBoardScreen from './screens/DashBoardScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +15,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="FoodLog" component={FoodLogScreen} />
         <Stack.Screen options={{ title: '' }} name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="DashBoard" component = {DashBoardScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
